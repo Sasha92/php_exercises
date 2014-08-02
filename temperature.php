86670ad05315ff55434c8a8ab602c3f38a56e183
Sasha92 <bobr2@ukr.net>
2014-08-02 10:04:00 +0300

    Solution temperature conversion

---
+++ temperature.php
@@ -0,0 +1,78 @@
+<?php
+
+// Create Temperature class that should have following methods:
+// getKelvin() - Returns temperature in Kelvin
+// getCelsius() - Returns temperature in Celsius
+// getFahrenheit() - Returns temperature in Fahrenheit
+// SetKelvin($degree) - sets temperature value in Kelvin
+// SetCelsius($degree) - sets temperature value in Celsius
+// SetFahrenheit($degree) - sets temperature value in Fahrenheit
+class Temperature
+{
+    private $degree;
+    private $kelvin;
+    private $fahrenheit;
+
+    public function setCelsius($degree)
+    {
+        $this->degree = $degree;
+    }
+
+    public function setKelvin($degree)
+    {
+        $this->kelvin = $degree;
+    }
+
+    public function setFahrenheit($degree)
+    {
+        $this->fahrenheit = $degree;
+    }
+
+    public function getCelsius()
+    {
+        if (isset($this->kelvin)) {
+            return $this->kelvin - 273.15;
+        } elseif (isset($this->fahrenheit)) {
+            return ($this->fahrenheit - 32) * 5 / 9;
+        } else {
+            return $this->degree;
+        }
+    }
+
+    public function getKelvin()
+    {
+        if (isset($this->degree)) {
+            return $this->degree + 273.15;
+        } elseif (isset($this->fahrenheit)) {
+            return ($this->fahrenheit + 459.67) * 5 / 9;
+        } else {
+            return $this->kelvin;
+        }
+    }
+
+    public function getFahrenheit()
+    {
+        if (isset($this->degree)) {
+            return $this->degree * 9 / 5 + 32;
+        } elseif (isset($this->kelvin)) {
+            return $this->kelvin * 9 / 5 - 459.67;
+        } else {
+            return $this->fahrenheit;
+        }
+    }
+
+}
+
+// The following check should pass after implementing temperature class
+$temperature = new Temperature();
+$temperature->setCelsius(100);
+
+if (
+    $temperature->getCelsius() === 100 &&
+    $temperature->getFahrenheit() == 212 &&
+    $temperature->getKelvin() == 373.15
+) {
+    print "Success" . PHP_EOL;
+} else {
+    print "Wrong value" . PHP_EOL;
+}

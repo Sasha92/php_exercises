30a02fca7053c10ce09d67115cbf501821bf2e65
Sasha92 <bobr2@ukr.net>
2014-08-25 11:54:19 +0300

    Solution date format

--- hw_3/dates.php
+++ hw_3/dates.php
@@ -2,8 +2,10 @@
 // Написать код приведения даты формата “01/18/2013 01:02:03”
 // к формату “2013-01-18 01:02:03”
 
-function dateFormat ($date) {
-    $format = date_create_from_format ('m/d/Y H:i:s', $date);
-    echo date_format ($format, 'Y-m-d H:i:s');
+function dateFormat($date)
+{
+    $format = date_create_from_format('m/d/Y H:i:s', $date);
+    echo date_format($format, 'Y-m-d H:i:s');
 }
+
 dateFormat('01/18/2013 01:02:03');

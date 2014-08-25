82df00d16ac4eef5d4c0a3c7cec5340a6dd34d27
Sasha92 <bobr2@ukr.net>
2014-08-25 17:55:27 +0300

    Solution find numbers in string

--- hw_3/manual_cast.php
+++ hw_3/manual_cast.php
@@ -4,3 +4,16 @@
  * непосредственно число, не используя стандартные функции приведения типов
  * (например “1252абв” в 1252)
  */
+function findNumber($str)
+{
+    $n = strlen($str);
+    if ($n > 0) {
+        preg_match_all("/\d+/", $str, $numbers);
+        foreach ($numbers as $number) {
+            foreach ($number as $num)
+                echo $num . PHP_EOL;
+        }
+    }
+}
+
+findNumber('1252abc');

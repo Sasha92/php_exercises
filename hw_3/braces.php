f1973e58e1246bc92a1e7587b64f67aff7e9fbe2
Sasha92 <bobr2@ukr.net>
2014-08-26 20:30:58 +0300

    Solution is correct string with braces

--- hw_3/braces.php
+++ hw_3/braces.php
@@ -5,9 +5,28 @@
  * надо определить, корректна ли строка,
  * т.е. для каждой закрывающей скобки должна быть своя открывающая.
  */
-function isCorrect()
+function isCorrect($brace)
 {
-    // write code here
+    $n = strlen($brace);
+    if (empty($brace)) {
+        return true;
+    } elseif ($n % 2 === 1) {
+        return false;
+    } elseif ($n > 0) {
+        // ( - a, )-b, {-c, }-d
+        $brace = str_replace('(', 'a', $brace);
+        $brace = str_replace(')', 'b', $brace);
+        $brace = str_replace('{', 'c', $brace);
+        $brace = str_replace('}', 'd', $brace);
+        for ($i = 0; $i < $n; $i++) {
+            $pattern = '/ab|cd/';
+            $brace = preg_replace($pattern, '', $brace);
+            if ($brace === '') return true;
+        }
+        if ($brace !== '') {
+            return false;
+        }
+    }
 }
 
 assert(isCorrect('') === true);

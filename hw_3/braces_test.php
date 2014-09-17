3c1c45d23a9beda57cbfcceeeebff038f83d82ae
Sasha92 <bobr2@ukr.net>
2014-09-17 20:33:26 +0300

    Performed hw3

---
+++ hw_3/braces_test.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'braces.php';
+
+
+/**
+ * @param $n
+ * @param $brace
+ */
+function testBrace($n, $brace)
+{
+    $start = microtime(true);
+    for ($i = 0; $i < $n; $i++) {
+        isCorrect($brace);
+    }
+
+    $end = microtime(true);
+    echo $end - $start;
+}
+
+testBrace(1000, generateIncorrectBraces(400));
+
+function generateIncorrectBraces($blocks)
+{
+    $string = '(}{)';
+    $str = '';
+    for ($i = 0; $i < $blocks; $i++) {
+        $str .= $string;
+    }
+    return $str;
+}
